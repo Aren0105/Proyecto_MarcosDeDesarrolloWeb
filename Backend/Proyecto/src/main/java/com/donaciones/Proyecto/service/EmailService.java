c0755d9203b7825657ff7a7fb8ed3ae8ae9496a1
Aren0105 <aren200501@gmail.com>
2026-06-10 13:41:35 -0500

    Update EmailService.java

--- Backend/Proyecto/src/main/java/com/donaciones/Proyecto/service/EmailService.java
+++ Backend/Proyecto/src/main/java/com/donaciones/Proyecto/service/EmailService.java
@@ -1,6 +1,5 @@
 package com.donaciones.Proyecto.service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
