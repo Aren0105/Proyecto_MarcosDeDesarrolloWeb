77b2ff4274f2c2de2022e6045c23c0f364383e2d
Aren0105 <aren200501@gmail.com>
2026-06-10 22:02:07 -0500

    Cambio de URL

--- Backend/Proyecto/src/main/java/com/donaciones/Proyecto/service/EmailService.java
+++ Backend/Proyecto/src/main/java/com/donaciones/Proyecto/service/EmailService.java
@@ -15,7 +15,7 @@ public class EmailService {
         message.setTo(destinatario);
         message.setSubject("Mano Solidaria - Restablecer contraseña");
 
-        String link = "http://127.0.0.1:5500/ManoSolidaria/reset-contrasenia.html?token=" + token;
+        String link = "http://127.0.0.1:5500/Frontend/ManoSolidaria/reset-contrasenia.html?token=" + token;
         System.out.println("LINK: " + link);
         message.setText("Haz clic en el siguiente enlace para restablecer tu contraseña:\n\n" + link +
                 "\n\nEste enlace expirará en 1 hora.\n\nSi no solicitaste este cambio, ignora este mensaje.");
